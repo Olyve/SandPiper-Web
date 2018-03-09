bd5c946b01b8ffcbe3a47af20d79fa78a6d05792
Sam Galizia <samgalizia@gmail.com>
2018-03-09 14:33:38 -0800

    Updated for CI setup
    Added Travis yaml file to run tests remotely on PR's. I also added
    the coverage and uploading of it to Code Climate for code coverage
    information. I moved the <BrowserRouter> tag into the app file too
    as the tests were complaining that it was not in the right place.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
         "es6": true,
         "jest": true
     },
-    "extends": "eslint:recommended",
+    "extends": [ 
+        "eslint:recommended",
+        "react-app"
+    ,
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import Auth from './components/Auth';
@@ -36,29 +36,31 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App container'>
-        <div className='grid-n'>
-          <ul>
-            <NavLink exact to='/' activeStyle={{fontWeight: 'bold'}} className='nav-item'>
-              Home
-            </NavLink>
-          </ul>
-        </div>
+      <BrowserRouter>
+        <div className='App container'>
+          <div className='grid-n'>
+            <ul>
+              <NavLink exact to='/' activeStyle={{fontWeight: 'bold'}} className='nav-item'>
+                Home
+              </NavLink>
+            </ul>
+          </div>
 
-        <div className='grid-l'>
-          <ul>
-            <NavLink exact to='/login' activeStyle={{fontWeight: 'bold'}} className='nav-item'>
-              Login
-            </NavLink>
-          </ul>
+          <div className='grid-l'>
+            <ul>
+              <NavLink exact to='/login' activeStyle={{fontWeight: 'bold'}} className='nav-item'>
+                Login
+              </NavLink>
+            </ul>
+          </div>
+          
+          <div className='grid-c'>
+            <Route exact path='/login' render={() => 
+              <Auth handleSignup={this.handleSignup} handleLogin={this.handleLogin} />
+            } />
+          </div>
         </div>
-        
-        <div className='grid-c'>
-          <Route exact path='/login' render={() => 
-            <Auth handleSignup={this.handleSignup} handleLogin={this.handleLogin} />
-          } />
-        </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
